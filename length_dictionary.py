07930bd62e9a2a6be661f00c92df3c8f63419829
Mradul-Modi <106447685+Mradul-Modi@users.noreply.github.com>
2022-05-31 22:15:37 +0530

    Create length_dictionary.py

---
+++ length_dictionary.py
@@ -0,0 +1,6 @@
+car={"brand":"audi","model":"R8","price":"8000000000","year":"2022","owner":"Akhil"}
+print("car detail:")
+forx in car.items():
+  print (x)
+  
+print("\n  length of car dictionary:",len(car))
